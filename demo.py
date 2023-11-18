344d41433b01f126c1dd7a08f30bfffc8a418bea
aman1100kumar <kumaraman0414@gmail.com>
2023-11-18 16:41:57 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def xyz(x,y):
+    z =  x + y
+    return z
+
+
+# In[2]:
+
+
+x = [123,345,56,67,8]
+x = [45,56,56,56]
+
+# In[ ]:
+
+
+y = {'a':12,'b':67}
+
